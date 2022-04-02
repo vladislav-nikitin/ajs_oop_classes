5b0ad47bdeb0f839a3b58ac79037aa7f6fb4709e
Vladislav Nikitin <vlad.nikitin_1991@mail.ru>
2022-04-02 20:27:27 +0300

    fixed Character.js

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,25 +1,22 @@
+const types = ["Bowman", "Swordsman", "Magician", "Daemon", "Undead", "Zombie"];
+
 export default class Character {
   constructor(name, type) {
     if (typeof name !== "string" || name.length < 2 || name.length > 10) {
       throw new Error(
         "Некорректные данные! name поддерживает только строковый тип данных. Длина name должна быть от 2 до 10 символов"
       );
-    } else if (
-      type !== "Bowman" &&
-      type !== "Swordsman" &&
-      type !== "Magician" &&
-      type !== "Daemon" &&
-      type !== "Undead" &&
-      type !== "Zombie"
-    ) {
-      throw new Error("Укажите корректный тип персонажа");
-    } else {
-      this.name = name;
-      this.type = type;
-      this.health = 100;
-      this.level = 1;
-      this.attack = attack;
-      this.defence = defence;
     }
+
+    if (!types.includes(type)) {
+      throw new Error("Укажите корректный тип персонажа");
+    }
+
+    this.name = name;
+    this.type = type;
+    this.health = 100;
+    this.level = 1;
+    this.attack = undefined;
+    this.defence = undefined;
   }
 }
